ca80998cf878e02fce59f11f0b9dd5b3c4682d81
Projjol Banerji <probaner23@gmail.com>
2018-11-23 14:31:39 +0530

    Fix tests (#5)
    To allow for build fixes in
    https://github.com/ngetahun/py-multistream-select/pull/2 some of the
    tests were broken, this commit fixes them.

--- test/test_utils.py
+++ test/test_utils.py
@@ -2,7 +2,7 @@ import pytest
 from multistream_select import utils
 
 @pytest.mark.parametrize(
-"test_in, test_out", [("hello, world", '0xd68656c6c6f2c20776f726c64a'),
-                              ("na", '0x36e61a'), ("ls", '0x36c73a')])
+"test_in, test_out", [("hello, world", '0x0d68656c6c6f2c20776f726c640a'),
+                              ("na", '0x036e610a'), ("ls", '0x036c730a')])
 def test_hexify(test_in, test_out):
     assert utils.hexify(test_in) == test_out
